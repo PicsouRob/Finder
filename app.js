6aeb1b772ab6cce8a61dccdbff53b93aea47bf3c
Phanord Roberto <phanordpicsouroberto11@gmail.com>
2021-10-16 23:53:46 -0400

    picsou

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ mongoose.connect(config.MONGOdb_ACCESS, { useNewUrlParser: true,
 );
 
 // Middleware.........
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
